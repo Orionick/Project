0e1bbe966184d1242436cb0702d1230233326b32
Orionick <72335975+Orionick@users.noreply.github.com>
2022-12-27 23:18:27 +0200

    Done bouncing ball

--- main.cpp
+++ main.cpp
@@ -11,12 +11,27 @@ int main() {
     CircleShape shape;
     shape.setRadius(50.0);
     shape.setFillColor(Color::Yellow);
+    shape.setOrigin(50.0, 50.0);
+    shape.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+    float xstep = 8, ystep = 4;
 
     while (window.isOpen() && !Keyboard::isKeyPressed(Keyboard::Escape)) {
         Event event;
         while (window.pollEvent(event))
             if (event.type == Event::Closed)
                 window.close();
+
+        shape.move(xstep, ystep);
+
+        if (shape.getPosition().x > window.getSize().x - 50)
+            xstep = -xstep;
+        else if (shape.getPosition().y > window.getSize().y - 50)
+            ystep = -ystep;
+        else if (shape.getPosition().x < 0 + 50)
+            xstep = -xstep;
+        else if (shape.getPosition().y < 0 + 50)
+            ystep = -ystep;
 
         window.clear();
         window.draw(shape);
